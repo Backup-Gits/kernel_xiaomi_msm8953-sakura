4a6cd6d2362d493ef6bafd6c556b06d3ccc69926
mahajant99 <mahajant99@gmail.com>
2020-04-16 13:25:37 +0530

    Add compile script

---
+++ compile.sh
@@ -0,0 +1,11 @@
+make clean && make mrproper
+export ARCH=arm64
+export CROSS_COMPILE=~/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+mkdir output
+make -C $(pwd) O=output sakura_defconfig
+make -j3 -C $(pwd) O=output
+git clone https://github.com/mahajant99/AnyKernel3.git -b sakura-aosp zip
+cp -r output/arch/arm64/boot/Image.gz-dtb zip/
+cd zip
+mv Image.gz-dtb zImage
+zip -r Test-q.zip *
